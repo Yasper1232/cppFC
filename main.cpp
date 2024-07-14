b35e62c0991ed9884ba56737d0366690f12f5994
Kacper <ciaparino123@gmail.com>
2024-07-14 20:44:02 +0200

    multiplication table using 2 for loops

--- main.cpp
+++ main.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main()
 {
-int grade,sum=0;
 
-    for(int i=0;i<3;i++) {
 
-        do {
+    //Multiplication table
 
-            cout<<"Enter grade "<<i+1<<": ";
-            cin>>grade;
+    for(int i=1;i<=10;i++) {
 
-        }while(grade<1 || grade>5);
-sum+=grade;
-    }   cout<<"Your average grade : "<<(float)sum/3.0;
+        for(int j=1;j<=10;j++) {
+
+                cout<<i<<" * "<<j<<" = "<<i*j<<endl;
+        }
+            cout<<endl;
+
+    }
 
     system("pause>0");
 }
