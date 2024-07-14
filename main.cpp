5b3c11b5c30191e34fd7992641007e5852ea079a
Kacper <ciaparino123@gmail.com>
2024-07-14 16:40:53 +0200

    petla while sprawdza ile cyfr ma liczba

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@ using namespace std;
 
 int main()
 {
-    //write out all the numbers between 100-500 that are divisible by 3 and 5
-int i=100;
-while(i<=500) {
+    //Count digit of a number
+int number;
+    cout<<"Number: ";
+    cin>>number;
 
-if((i%3==0)&&(i%5==0)) {
+    if(number==0)
+        cout<<" You have entered 0.\n";
+    else {
+//1325 -> counter=0
+        //132 -> counter=1
+        //13 -> counter=2 ... 1 -> counter=3 -> counter=4
 
-    cout<<i<<" is divisible by 3 and 5 "<<endl;
+        int counter=0;
+        while(number>0) {
 
-}
-i++;
-}
+            number=number/10; //np 123 -> 12 -> 2 -> 0
+                // moglibysmy rowniez napisac : number /=10
+            counter++;
+        }
+        cout<<" Number contains: "<<counter<<" digits\n";
+
+    }
 
     system("pause>0");
 }
