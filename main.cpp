89fbdcc93b66447be333005cd095b429e2155c9b
Kacper <ciaparino123@gmail.com>
2024-07-16 22:47:22 +0200

    function overloading

--- main.cpp
+++ main.cpp
@@ -2,32 +2,37 @@
 #include <iomanip>
 using namespace std;
 
-bool isPrimeNumber(int number) {
-    for(int i=2;i<number;i++) {
-
-        if(number%i==0) {
-            return false;
-        }
-
-    }
-    return true;
-}
+int sum(int a,int b);
+double sum(double a,double b);
+float sum(float a,float b,float c);
 
 int main()
 {
+//function overloading
 
+cout<<sum(4,3)<<endl;
+cout<<sum(4.4,3.3)<<endl;
+    cout<<sum(4.4,3.3,2.2);
 
-    for(int i=1;i<=100000;i++) {
-
-        if(isPrimeNumber(i)) {
-
-            cout<<i<<" is prime number\n";
-
-        }
-
-    }
 
     system("pause>0");
 }
 
+int sum(int a,int b) {
 
+    return a+b;
+
+}
+
+double sum(double a,double b) {
+
+
+return a+b;
+
+}
+
+float sum(float a,float b,float c) {
+
+return a+b+c;
+
+}
