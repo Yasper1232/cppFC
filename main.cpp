2bb247a8dbc2cf9e91dc87e65f347855f98d7a03
Kacper <ciaparino123@gmail.com>
2024-07-17 21:34:30 +0200

    Generic function and templates

--- main.cpp
+++ main.cpp
@@ -2,48 +2,33 @@
 #include <iomanip>
 using namespace std;
 
-void showMenu() {
+template<typename T>
 
-cout<<"**********MENU**********"<<endl;
-    cout<<"1.Check balance"<<endl;
-    cout<<"2.Deposit"<<endl;
-    cout<<"3.Withdraw"<<endl;
-    cout<<"4.EXIT"<<endl;
-    cout<<"************************"<<endl;
+void Swap(T& a,T& b) {
+
+T temp=a;
+    a=b;
+    b=temp;
 
 }
 
+//uzywajac template nie trzeba powtarzac kodu wiele razy , template dziala na kazdy typ danych
+
+
+
 int main()
 {
-int option;
-double balance=500;
 
-    //ATM app
-//check balance, deposit money,withdraw money,show menu
-do {
-    showMenu();
-    cout<<"Please choose an option: ";
-    cin>>option;
-system("cls");
-    switch (option) {
+            int a=5,b=7;
 
-        case 1:cout<<"Balance is: "<<balance<<" $"<<endl; break;
-        case 2:cout<<"Deposit amount:";
-        double depositAmount;
-        cin>>depositAmount;
-        balance+=depositAmount; break;
-        case 3:cout<<"Withdraw amount:";
-        double WithdrawAmount;
-        cin>>WithdrawAmount;
-        if(WithdrawAmount<=balance)
-            balance-=WithdrawAmount;
-        else {
-            cout<<"Not enough money"<<endl;
-        }
-        break;
-    }
-}while (option!=4);
+    cout<<a<<" - "<<b<<endl;
+    Swap<int>(a,b);
+    cout<<a<<" - "<<b<<endl;
 
+char c='c',d='d';
+    cout<<c<<" - "<<d<<endl;
+Swap<char>(c,d);
+    cout<<c<<" - "<<d<<endl;
 
     system("pause>0");
 }
